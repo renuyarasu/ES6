03018cf3d1088a4117dc65661b35782d6bd8e544
Renuka Prasad <renu.yarasu@gmail.com>
2021-09-29 21:59:44 +0530

    Destructuring - Objects

--- script.js
+++ script.js
@@ -6,9 +6,31 @@ const name = 'Renuka Prasad Yarasu';
 const nameArr = name.split(' ');
 /* const fName = nameArr[0];
 const mName = nameArr[1];
-const lName = nameArr[2]; */
+const lName = nameArr[2]; 
 console.log(nameArr);
 const [fName, lName, ,] = nameArr;
 console.log(fName);
 console.log(lName);
+*/
+// Destructuring - Objects
 
+/* const person = {
+    fName: 'Renuka',
+    mName: 'Prasad',
+    lName: 'Yarasu',
+}
+const {fName, lName} = person;
+console.log(fName);
+console.log(lName);
+ */
+
+let fNmae = 'Renuka';
+let mName = 'Prasad';
+let lName = 'Yarasu';
+
+const person = {
+    fNmae,
+    mName,
+    lName
+}
+console.log(person);
