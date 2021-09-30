be96b86e3d256238970721c2d895aa50727aa367
Renuka Prasad <renu.yarasu@gmail.com>
2021-09-30 13:23:27 +0530

    Array Functions: filter() - Odd Numbers

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 let result = numbers.filter((item) => {
     // console.log(item);
-    return item % 2 === 0
+    return item % 2 !== 0
 })
 
 console.log('Result Array: ', result);
