73c99cb454a946d14041042b1cd766ef8ed3734c
Renuka Prasad <renu.yarasu@gmail.com>
2021-09-30 13:23:10 +0530

    Array Functions: filter() - Even Numbers

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ console.clear();
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 let result = numbers.filter((item) => {
-    console.log(item);
+    // console.log(item);
+    return item % 2 === 0
 })
 
 console.log('Result Array: ', result);
