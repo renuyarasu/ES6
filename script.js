b4b12204466d17f50ebc30e9fb657a874b802ea4
Renuka Prasad <renu.yarasu@gmail.com>
2021-09-30 13:42:43 +0530

    Array Functions: find()

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 console.clear();
-// ES6 - Array Functions: filter()
+// ES6 - Array Functions: find() and findIndex()
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numbers = [-1, -2, -3, -4, -5, 1, 2, 3, 4, 5];
 
-let result = numbers.filter((item) => {
-    // console.log(item);
-    return item % 2 !== 0
+let results = numbers.find((item) => {
+    return (item % 2 === 0 && item >= 2)
 })
 
-console.log('Result Array: ', result);
+console.log('Results: ', results);
