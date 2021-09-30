7808879e10dda35b5ed4e47e1741fc8a6323aeee
Renuka Prasad <renu.yarasu@gmail.com>
2021-09-30 13:04:17 +0530

    Array Functions: map()

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
 console.clear();
-// ES6 - Destructuring
+// ES6 - Array Functions: map()
 
-// Destructuring - Arrays
-const name = 'Renuka Prasad Yarasu';
-const nameArr = name.split(' ');
-/* const fName = nameArr[0];
-const mName = nameArr[1];
-const lName = nameArr[2]; 
-console.log(nameArr);
-const [fName, lName, ,] = nameArr;
-console.log(fName);
-console.log(lName);
-*/
-// Destructuring - Objects
+let myArr = [1, 2, 3, 4, 5];
 
-/* const person = {
-    fName: 'Renuka',
-    mName: 'Prasad',
-    lName: 'Yarasu',
-}
-const {fName, lName} = person;
-console.log(fName);
-console.log(lName);
- */
+let newArr = myArr.map((data, pos) => {
+    console.log(`Item at pos: ${pos} is ${data*2}`);
+    // console.log(data * 2);
+})
 
-let fNmae = 'Renuka';
-let mName = 'Prasad';
-let lName = 'Yarasu';
-
-const person = {
-    fNmae,
-    mName,
-    lName
-}
-console.log(person);
+console.log(newArr);
